8bd9d4dd3c77a5edbf4b1df520beb140075e2ccf
Your Names <you@example.com>
2020-03-08 23:55:33 +0600

    update

--- lab3/lab3/CreateViewController.swift
+++ lab3/lab3/CreateViewController.swift
@@ -9,22 +9,84 @@
 import UIKit
 
 class CreateViewController: UIViewController {
-
+    
+    @IBOutlet weak var widthText: UITextField!
+    @IBOutlet weak var heightText: UITextField!
+    @IBOutlet weak var xcordText: UITextField!
+    @IBOutlet weak var ycordText: UITextField!
+    
+    @IBOutlet var colorOfButtons: [UIButton]!
+     var choosecolor:UIColor!
+    var selectedView: UIView?
     override func viewDidLoad() {
-        super.viewDidLoad()
+         super.viewDidLoad()
+        
+   Navbutton()
+}
+    func Navbutton(){
+           
+              let addButton = UIBarButtonItem(title: "save", style: .plain, target: self, action: #selector(addframe))
+              navigationItem.rightBarButtonItems = [ addButton]
+             
+       }
 
-        // Do any additional setup after loading the view.
+    @IBAction func colorDetect(_ sender: UIButton) {
+        
+        colorOfButtons.forEach{(button) in
+            if button == sender{
+                button.isSelected=true
+            }
+            else{
+                button.isSelected=false
+            }
+        }
+        guard let btncolor = sender.backgroundColor else {return}
+         choosecolor = btncolor
+        
     }
     
+    var onSelect: ((_ widthText:Double, _ heightText: Double, _ xcordText:Double, _ ycordText:Double, _ chooosecolor:UIColor) -> Void)? = nil
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func addframe() {
+        
+        guard let width = widthText.text, let height = heightText.text, let x = xcordText.text, let y = ycordText.text, let color = choosecolor else {
+            let alert = UIAlertController(title: "Error", message: "Please write all data!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Done, frame added", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        guard let realwidth = Double(width), let realheight = Double(height), let realx = Double(x), let realy = Double(y) else {
+            let alert = UIAlertController(title: "Error", message: "Data type conflict", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Everything OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        let screenSizeofView = UIScreen.main.bounds
+        if CGFloat(realwidth) > screenSizeofView.width || CGFloat(realheight) > screenSizeofView.height {
+            let alert = UIAlertController(title: "Error", message: "Data is bigger than Mainview", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        guard let figure = selectedView else {
+            onSelect?(realwidth, realheight, realx, realy, choosecolor)
+            self.navigationController?.popViewController(animated: true)
+            return
+        }
+        
+       figure.backgroundColor = color
+        figure.frame = CGRect(x: CGFloat(realx), y: CGFloat(realy), width: CGFloat(realwidth), height: CGFloat(realheight))
+        
+self.navigationController?.popViewController(animated: true)
     }
-    */
+   
+    
+       
+      
+    }
 
-}
+
--- lab3/lab3/ViewController.swift
+++ lab3/lab3/ViewController.swift
@@ -10,11 +10,76 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var someView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+        @IBAction func createData(_ sender: Any) {
+            
+      CreateView()
+      
+    }
+    
+    
+    @objc func CreateView() {
+           
+           let storyboard = UIStoryboard(name: "Main", bundle: nil)
+           let vc = storyboard.instantiateViewController(identifier: "createview") as CreateViewController
+           
+           vc.onSelect = { [weak self] width, height, x, y, color in
+               guard let self = self else { return }
+               
+               let figure = UIView(frame: CGRect(x: x, y: y, width: width, height: height))
+               figure.backgroundColor = color
+               self.view.addSubview(figure)
+               
+               let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.didTap))
+               figure.addGestureRecognizer(tapGestureRecognizer)
+               
+               let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.didPan(recognizer:)))
+               figure.addGestureRecognizer(panGestureRecognizer)
+               
+               let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(self.didPinch))
+               figure.addGestureRecognizer(pinchGestureRecognizer)
+           }
+           navigationController?.pushViewController(vc, animated: true)
+       }
+       
+    
+  
+        
+    @objc func didTap() {
+     let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                 guard
+          let selectVC = storyboard.instantiateViewController(identifier: "createview") as? CreateViewController
+                 else { return }
+     navigationController?.pushViewController(selectVC, animated: true)
+    
+       }
+    @objc func didPinch(_ sender: UIPinchGestureRecognizer) {
+        if let figure = sender.view {
+            if sender.state == .began || sender.state == .changed {
+                figure.transform = (figure.transform.scaledBy(x: sender.scale, y: sender.scale))
+               sender.scale = 1.0
+            }
+        }
+    }
+    
+    
+    var baseOrigin: CGPoint!
+      @objc func didPan(recognizer: UIPanGestureRecognizer) {
+          if let figure = recognizer.view {
+              switch recognizer.state {
+              case .began:
+                  baseOrigin = figure.frame.origin
+              case .changed:
+                  let d = recognizer.translation(in: figure)
+                  figure.frame.origin.x = baseOrigin.x + d.x
+                  figure.frame.origin.y = baseOrigin.y + d.y
+              default: break
+              }
+          }
+      }
 
 }
-
